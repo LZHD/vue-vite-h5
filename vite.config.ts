98e9c886cdd04724c5c9639e8ace9573ef3f73c8
LZHD <lzhdfxq@163.com>
2021-06-29 00:09:07 +0800

    chore: base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import styleImport from 'vite-plugin-style-import';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
+function resolve(dir: string) {
+  return path.join(__dirname, dir);
+}
 // https://vitejs.dev/config/
 // https://github.com/vitejs/vite/issues/1930 .env
 export default defineConfig({
@@ -16,5 +21,14 @@ export default defineConfig({
         }
       ]
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': resolve('./src'),
+      '@common': resolve('./src/common'),
+      '@components': resolve('./src/components'),
+      '@store': resolve('./src/store'),
+      '@views': resolve('./src/views')
+    }
+  }
 });
