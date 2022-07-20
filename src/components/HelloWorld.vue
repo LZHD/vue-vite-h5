e1cea06998ae58cb31e816ba1c2fb12d98c9e8db
LZHD <lzhdfxq@163.com>
2022-07-20 23:27:31 +0800

    chore: modify the import method of vant

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,11 +32,11 @@
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
-  <van-button type="primary">主要按钮</van-button>
-  <van-button type="success">成功按钮</van-button>
-  <van-button type="default">默认按钮</van-button>
-  <van-button type="warning">警告按钮</van-button>
-  <van-button type="danger">危险按钮</van-button>
+  <van-button type="primary">primary</van-button>
+  <van-button type="success">success</van-button>
+  <van-button type="default">default</van-button>
+  <van-button type="warning">warning</van-button>
+  <van-button type="danger">danger</van-button>
 </template>
 
 <script lang="ts">
